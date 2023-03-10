a6715214f4c76dc456af2392d49e050f8c446741
Ed Bueler <elbueler@alaska.edu>
2023-03-09 16:21:56 -0900

    Matlab ode23 and ode45 do not behave as expected

--- assets/codes/stepsizes.m
+++ assets/codes/stepsizes.m
@@ -3,24 +3,25 @@ function stepsizes(tol)
 % of ODE IVP  u'(t) = 2 u(t) + exp(-t),  u(0) = 1
 % Compare actual error at t=2 to requested RelTol.
 
-if nargin < 1,  tol = 1.0e-4;  end
-
 % problem
 f = @(t,u) 2*u + exp(-t);
 u0 = 1.0;  tf = 2.0;
+uexactf = (4/3) * exp(2*tf) - (1/3) * exp(-tf);
 
 % solve with black boxes and plot step sizes
+% note that diff(t23) are the stepsizes from ode23()
+if nargin < 1,  tol = 1.0e-4;  end
 [t23,u23] = ode23(f,[0,tf],u0,odeset('RelTol',tol,'AbsTol',tol));
 [t45,u45] = ode45(f,[0,tf],u0,odeset('RelTol',tol,'AbsTol',tol));
 plot(t23(2:end),diff(t23),'o',t45(2:end),diff(t45),'o')
 xlabel t,  ylabel('adaptive time step k_n')
 legend('ode23','ode45')
-axis([0 tf 0 1.3*max(abs(diff(t45)))])
+maxy = max([abs(diff(t45)); abs(diff(t23))]);
+axis([0 tf 0 1.3*maxy])
 
-% compute U^N - uexact(tf)
-uexact = @(t) (4/3) * exp(2*t) - (1/3) * exp(-t);
-printf('for tol=%.3e:\n',tol)
-printf('  ode23 takes %4d steps with final error |U^N-u(tf)|/|u(tf)| = %.3e\n',...
-       length(t23)-1, abs(u23(end) - uexact(tf)) / abs(uexact(tf)))
-printf('  ode45 takes %4d steps with final error |U^N-u(tf)|/|u(tf)| = %.3e\n',...
-       length(t45)-1, abs(u45(end) - uexact(tf)) / abs(uexact(tf)))
+% compute actual errors
+fprintf('for tol=%.3e:\n',tol)
+fprintf('  ode23: %4d steps, actual relative final error = %.3e\n',...
+        length(t23)-1, abs(u23(end) - uexactf) / abs(uexactf))
+fprintf('  ode45: %4d steps, actual relative final error = %.3e\n',...
+        length(t45)-1, abs(u45(end) - uexactf) / abs(uexactf))
