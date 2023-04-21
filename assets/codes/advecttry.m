ff41fe649925fe081765992d95407f1300d6cd4e
Ed Bueler <elbueler@alaska.edu>
2023-04-21 10:15:44 -0800

    post demo code

---
+++ assets/codes/advecttry.m
@@ -0,0 +1,42 @@
+% ADVECTTRY  Demonstrate FTCS and Lax-Friedrichs on
+%   u_t + 0.5 u_x = 0
+% over t,x in [0,2]x[0,1] with square initial condition
+% and periodic boundary conditions.  Generates three
+% figures.
+
+a = 0.5;
+tf = 2.0;
+for m = [20 80 320]
+    h = 1.0 / m;
+    k = 0.9 * (h / abs(a));   % safety factor: nu = ak/h = 0.9 < 1
+    % k = h / abs(a);         % suspicious result?
+    NN = ceil(tf / k)
+    k = tf / NN;
+    nu = a * k / h;           % Courant number
+    x = 0.0:h:1.0-h;
+    u0 = ones(size(x));
+    u0(x < 0.4) = 0.0;
+    u0(x > 0.6) = 0.0;
+    uexact = u0;              % why? (note tf matters!)
+    % FTCS
+    U = u0;
+    for n = 1:NN
+        U = U - (nu/2) * ([U(2:m) U(1)] - [U(1:m-1) U(m)]);
+    end
+    Uftcs = U;
+    % LF
+    U = u0;
+    for n = 1:NN
+        U = 0.5 * ([U(2:m) U(1)] + [U(1:m-1) U(m)]) ...
+            - (nu/2) * ([U(2:m) U(1)] - [U(1:m-1) U(m)]);
+    end
+    Ulf = U;
+    norm(Uftcs - uexact,2)
+    norm(Ulf - uexact,2)
+    figure()
+    plot(x,Uftcs,'o-',x,Ulf,'o-',x,uexact,'r')
+    axis([0 1 -0.5 1.5])
+    xlabel x
+    legend('FTCS','Lax-Friedrichs',sprintf('u(%.2f,x)',tf))
+    title(sprintf('m=%d',m))
+end
